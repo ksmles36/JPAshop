8c9a7d0691e8ee22072fea2ace5a87858ebcf47c
ksmles36 <ksmles36@naver.com>
2023-01-05 22:23:09 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/jpashop/JpAshopApplication.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpAshopApplication.class, args);
+    }
+
+}
