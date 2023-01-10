b47b1f63bdd04ad9f3b623af828ca61bd1e2c927
ksmles36 <ksmles36@gamil.com>
2023-01-11 01:08:26 +0900

    a

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -26,6 +26,7 @@ public class MemberApiController {
     @GetMapping("/api/v2/members")
     public Result membersV2() {
         List<Member> findMembers = memberService.findMembers();
+
         List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
         return new Result(collect);
     }
