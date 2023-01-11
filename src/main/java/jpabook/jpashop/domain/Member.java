8244528df5c130d22e120f0f3e45ab1fe07179f1
ksmles36 <ksmles36@gmail.com>
2023-01-11 23:02:32 +0900

    샘플데이터 만들기

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.tomcat.jni.Address;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
