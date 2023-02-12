b493c766913b002e6a70a56e45679bcc19ae4f57
ksmles36 <ksmles36@gmail.com>
2023-02-13 00:20:52 +0900

    간단한 주문 조회 V4 실습 dto로 직접 조회 완료

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -2,9 +2,11 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import jpabook.jpashop.repository.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +27,7 @@ import java.util.stream.Collectors;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -55,19 +58,21 @@ public class OrderSimpleApiController {
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
         private Address address;
 
         public SimpleOrderDto(Order order) {
             this.orderId = order.getId();
             this.name = order.getMember().getName();  //LAZY 초기화
             this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
             this.address = order.getDelivery().getAddress();  //LAZY 초기화
         }
     }
 
     @GetMapping("/api/v4/simple-orders")
     public List<OrderSimpleQueryDto> ordersV4() {
-        return orderRepository.findOrderDtos();
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
 
